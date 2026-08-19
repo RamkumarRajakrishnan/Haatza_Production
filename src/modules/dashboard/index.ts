cc0e6dd717a89414f80e6fb4dadd3dc5c7338adb
Arungovindasamy <arungovindasamy99@gmail.com>
2026-08-19 16:29:58 +0530

    arch: standardize module index exports for Auth and Dashboard modules

---
+++ src/modules/dashboard/index.ts
@@ -0,0 +1,4 @@
+export * from './dashboard.module';
+export * from './controllers/dashboard.controller';
+export * from './services/dashboard.service';
+export * from './dto/get-haatza-dashboard.dto';
